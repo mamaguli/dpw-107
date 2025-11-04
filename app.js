4f981619a76ae4637654164766540e1e69f0c850
mamaguli <85080794+mamaguli@users.noreply.github.com>
2025-11-04 10:16:36 -0400

    cafe
    sitio cafe

---
+++ app.js
@@ -0,0 +1,49 @@
+// datos json
+const productosJSON = [
+  {
+    id: 1,
+    nombre: "Cafe MOla",
+    precio: 70.00,
+    descripcion: "Exquisito cafe con chocolate y crema",
+    imagen: "imagenes/te-verde.jpg"
+  },
+  {
+    id: 2,
+    nombre: "Te Verde",
+    precio: 50.00,
+    descripcion: "Hoja de te verde",
+    imagen: "/imagenes/te-verde.jpg"
+  },
+  {
+    id: 3,
+    nombre: "Smoothie de Frutas",
+    precio: 40.00,
+    descripcion: "Mezcla de Frutas",
+    imagen: "imagenes/te-verde.jpg"
+  }
+  
+]
+// funcion principal
+function mostrarProductos(){
+  const contenedor = document.querySelector('#contenedor-productos')
+  //recorrer
+  productosJSON.forEach(productos => {
+      //desde javascript se crearar un contenedor div
+      const card = document.createElement('div')
+      //asignar al contenedor una clase
+      card.classList.add('productos-card')
+      //construir la tarjera
+      card.innerHTML =  `
+        
+         <img src=${productos.imagen}>
+        <h2>${productos.nombre}</h2>
+        <p>${productos.descripcion}</p>
+        <button data-id="${productos.id}">Agregar al carrito</button>
+      `
+    //inserta el nuevo elemento a la tarjeta
+      contenedor.appendChild(card);    
+  })
+
+}
+// llamar a la funcion
+mostrarProductos();
